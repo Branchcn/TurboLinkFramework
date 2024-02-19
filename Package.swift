b9323c6e8be824b3f899d889864d250e01de3d42
logan <logan.li@branchcn.com>
2024-02-19 10:17:43 +0800

    init project

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TurboLinkSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TurboLinkSDK",
+            targets: ["TurboLinkSDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "TurboLinkSDK",
+            url: "https://github.com/Branchcn/TurboLinkFramework/releases/download/1.0.0/TurboLinkSDK.xcframework.zip",
+            checksum: "56d3d282558d2b5b3f8e7e2f76c2b861e99257191140969b02c088869d11895e")
+    ]
+)
